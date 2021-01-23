8828509cf4a348d60d86d43c6618d2471e4d0baf
thanhnguyen0908 <59215823+thanhnguyen0908@users.noreply.github.com>
2021-01-23 21:52:52 +0700

    Upload up to lab 15

--- USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -1,26 +1,36 @@
 package vn.edu.usth.weather;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
-import androidx.viewpager.widget.ViewPager;
-
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.ImageRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.tabs.TabLayout;
 
 public class WeatherActivity extends AppCompatActivity {
 
-    WeatherAndForecastFragment weatherAndForecastFragment;
     ViewPager viewPager;
     TextView pagination;
     int total = 4, count = 1;
 
+    @SuppressLint("SetTextI18n")
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
 
@@ -32,6 +42,8 @@ public class WeatherActivity extends AppCompatActivity {
         viewPager.setAdapter(new SliderAdapter(getSupportFragmentManager()));
 
         pagination.setText(count + "/" + total);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
             @Override
@@ -39,6 +51,7 @@ public class WeatherActivity extends AppCompatActivity {
 
             }
 
+            @SuppressLint("SetTextI18n")
             @Override
             public void onPageSelected(int position) {
                 count = 0;
@@ -51,7 +64,10 @@ public class WeatherActivity extends AppCompatActivity {
 
             }
         });
+
     }
+
+
 
     @Override
     protected void onStart() {
@@ -83,4 +99,36 @@ public class WeatherActivity extends AppCompatActivity {
         Log.i(getClass().getName(), "Weather Activity onDestroy");
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.refresh:
+
+                RequestQueue requestQueue = Volley.newRequestQueue(this);
+                Response.Listener<Bitmap> responseListener = response -> {
+                    ImageView logo = findViewById(R.id.logo);
+                    logo.setImageBitmap(response);
+                };
+
+                ImageRequest imageRequest = new ImageRequest(
+                        "https://usth.edu.vn/uploads/logo_moi-eng.png", responseListener,
+                        0, 0, ImageView.ScaleType.CENTER, Bitmap.Config.ARGB_8888, null);
+
+                requestQueue.add(imageRequest);
+                return true;
+            case R.id.settings:
+                startActivity(new Intent(WeatherActivity.this, PrefActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
 }
